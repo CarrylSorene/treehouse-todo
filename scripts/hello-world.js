a98cbc98c87966af331ad177b163d0412ad5ca38
Carryl Sorene <carrylsorene@gmail.com>
2016-03-07 12:51:03 +0000

    Removed hello-world example. Added todo list to index.html

--- scripts/hello-world.js
+++
@@ -1,9 +0,0 @@
-//no [] so it looks for existing app
-angular.module("todoListApp")
-.directive("helloWorld", function() {
-  return {
-    template: "This is the hello world directive",
-    restrict: "E"
-  // E to restrict as an element only, not an attribute
-  };
-});
