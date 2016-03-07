94492232732e3e38bff8075921d9daf26095931f
Carryl Sorene <carrylsorene@gmail.com>
2016-03-07 12:41:46 +0000

    Created custom directive

--- scripts/hello-world.js
+++ scripts/hello-world.js
@@ -2,6 +2,8 @@
 angular.module("todoListApp")
 .directive("helloWorld", function() {
   return {
-    template: "This is the hello world directive"
+    template: "This is the hello world directive",
+    restrict: "E"
+  // E to restrict as an element only, not an attribute
   };
 });
