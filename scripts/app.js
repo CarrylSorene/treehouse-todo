5958a7b4c00e10a15e75d324a99e5889fd6151e8
Carryl Sorene <carrylsorene@gmail.com>
2016-03-07 14:32:01 +0000

    Added show, hide and repeat directives

--- scripts/app.js
+++ scripts/app.js
@@ -4,4 +4,14 @@ angular.module("todoListApp", [])
   $scope.helloWorld = function() {
     console.log("Hello there. This is the helloWorld controller function in the mainCtrl.");
   };
+
+  $scope.todos = [
+    {"name": "Make an app"},
+    {"name": "Do yoga"},
+    {"name": "Draw someone"},
+    {"name": "Watch Phil's new video"},
+    {"name": "Feed the cat"},
+    {"name": "Clean the kitchen"}
+  ]
+
 });
