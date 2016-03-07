2bca05479bd684554895f9aa47af30f5e13dbe81
Carryl Sorene <carrylsorene@gmail.com>
2016-03-07 15:56:16 +0000

    Added functions to delete and save

--- scripts/app.js
+++ scripts/app.js
@@ -1,25 +1,43 @@
 // [] for dependencies
 angular.module("todoListApp", [])
 .controller("mainCtrl", function($scope, dataService) {
-  $scope.helloConsole = dataService.helloConsole;
-  // $scope.learningNgChange = function() {
-  //   console.log("An input changed");
-  // };
+  $scope.learningNgChange = function() {
+    console.log("An input changed.");
+  };  
 
-  $scope.todos = [
-    {"name": "Make an app"},
-    {"name": "Do yoga"},
-    {"name": "Draw someone"},
-    {"name": "Watch Phil's new video"},
-    {"name": "Feed the cat"},
-    {"name": "Clean the kitchen"}
-  ]
+  $scope.helloWorld = dataService.helloWorld;
+
+  dataService.getTodos(function(response) {
+    console.log(response.data);
+    $scope.todos = response.data;
+  });
+
+  $scope.deleteTodo = function(todo, $index) {
+    dataService.deleteTodo(todo);
+    $scope.todos.splice($index, 1);
+  };
+
+  $scope.saveTodo = function(todo) {
+    dataService.saveTodo(todo);
+  };
 
 })
-.service("dataService", function() {
+.service("dataService", function($http) {
+  this.helloWorld = function() {
+    console.log("This is the data service's method.");
+  };
+  
+  this.getTodos = function(callback){
+    $http.get("mock/todos.json")
+    .then(callback) 
+  };
 
-  this.helloConsole = function() {
-    console.log("This is the hello console service.");
-  }
+  this.deleteTodo = function(todo) {
+    console.log("The " + todo.name + "todo has been deleted.")
+  };
+
+  this.saveTodo = function(todo) {
+    console.log("The " + todo.name + "todo has been saved.")
+  };
 
 });
